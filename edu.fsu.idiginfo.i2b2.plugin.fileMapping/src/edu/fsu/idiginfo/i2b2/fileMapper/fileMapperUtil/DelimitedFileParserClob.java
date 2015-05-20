39a4e336fd5e6255fae95af66f0d4a243161281e
gmacdonnell <gmacdonnell@fsu.edu>
2015-05-20 08:16:50 -0400

    Save of new tmh parsing java

--- edu.fsu.idiginfo.i2b2.plugin.fileMapping/src/edu/fsu/idiginfo/i2b2/fileMapper/fileMapperUtil/DelimitedFileParserClob.java
+++ edu.fsu.idiginfo.i2b2.plugin.fileMapping/src/edu/fsu/idiginfo/i2b2/fileMapper/fileMapperUtil/DelimitedFileParserClob.java
@@ -31,19 +31,20 @@ public class DelimitedFileParserClob implements IFileParser {
 	protected String[] Delimiters;
 	protected int FieldCount;
 	protected int RowCount;
-	private static String ERROR_LOG_EXT = "_error.log";
-	private static String XML_EXT=".xml";
+	private static String ERROR_LOG_EXT = "_error.log.csv";
+	private static String XML_EXT = ".xml";
 	public static String COMMA = ",";
 	public static char QUOTE = '\"';
 	public static String AND = ";";
 	public static String NOT_NUM = "[\\D]+";
 	public static String BLANK = "";
 	public static String SPACE = " ";
-	public static String UNDERSCORE="_";
+	public static String UNDERSCORE = "_";
 	public static String NEW_RECORD = "\\r\\d*[^A-z]";
-	public static String ILLLEGAL_CHARS="[^A-z_,\\d\\s:.?;]";
+	public static String ILLLEGAL_CHARS = "[^A-z_,\\d\\s:.?;]";
 	public static String NEW_LINE = System.getProperty("line.separator");
 	public static int CHUNCK = 100;
+	public static int TAIL_LENGTH = 6;
 	public static int HSP_MD_NUM = 0;
 	public static int CHF_CMPLN = 1;
 	public static int ENCOUNTERDIAGDESCR = 2;
@@ -57,10 +58,11 @@ public class DelimitedFileParserClob implements IFileParser {
 	public static int VISIT_ID = 10;
 	public static int VISIT_STATUS = 11;
 	public static int FIELD_BREAKS = 9;
-	public static int NUM_FIELDS=10;
+	public static int NUM_FIELDS = 10;
 	public static int GOOD_COUNT = 0;
 	public static int BAD_COUNT = 1;
-	private static  String[] NTK_TYPE= { "HEAVY", "MODERATE", "OCCASIONAL", "NONE" };
+	private static String[] NTK_TYPE = { "HEAVY", "MODERATE", "OCCASIONAL",
+			"NONE" };
 	private static boolean DEBUG = false;
 	private static Integer[] max_lengths = new Integer[NUM_FIELDS];
 
@@ -113,18 +115,31 @@ public class DelimitedFileParserClob implements IFileParser {
 				dout.delete();
 				dout = null;
 			}
+			double start, end;
+			start = System.currentTimeMillis()/1000;
 			int[] count = cleanRows(InFile, OutFile);
+			end = System.currentTimeMillis()/1000;
+			System.out.println("Cleaning rows elapsed time: " + new Double( end- start).toString());
 			System.out.println("Total rows cleaned: " + count[GOOD_COUNT]);
 			System.out.println("Total Error Records: " + count[BAD_COUNT]);
+			start = System.currentTimeMillis()/1000;
 			int[] vcount = validateRows(OutFile, VOutFile);
+			end = System.currentTimeMillis()/1000;
+			System.out.println("Validating rows elapsed time: " + new Double( end- start).toString());
 			System.out.println("Total rows  " + vcount[GOOD_COUNT]);
 			System.out.println("Total invalid rows: " + vcount[BAD_COUNT]);
+			start = System.currentTimeMillis()/1000;
 			int[] ccount = parseComplaint(VOutFile, COutFile);
+			end = System.currentTimeMillis()/1000;
+			System.out.println("Parsing rows elapsed time: " + new Double( end- start).toString());
 			System.out.println("Total rows into parseComplaint: "
 					+ ccount[GOOD_COUNT]);
 			System.out.println("Total Error Records: " + ccount[BAD_COUNT]);
+			start = System.currentTimeMillis()/1000;
 			int[] dcount = removeDuplicateRecords(COutFile, DOutFile);
-			System.out.println("Total rows into Cleaned File: "
+			end = System.currentTimeMillis()/1000;
+			System.out.println("Removing duplicate rows elapsed time: " + new Double( end- start).toString());
+			System.out.println("Total rows into parsed File: "
 					+ dcount[GOOD_COUNT]);
 			System.out.println("Total Duplicates: " + dcount[BAD_COUNT]);
 		} catch (Exception e) {
@@ -200,52 +215,60 @@ public class DelimitedFileParserClob implements IFileParser {
 	public boolean hasID(String line) {
 		return false;
 	}
-	private static void makeXML(TMHVisit visit)
-	{
-		//TODO write code to add visit to a container class
-		
+
+	private static void makeXML(TMHVisit visit) {
+		// TODO write code to add visit to a container class
+
 	}
-	private static void cleanHeader(String header)
-	{
+
+	private static void cleanHeader(String header) {
 		header = header.replaceAll(SPACE, UNDERSCORE);
 		HEADER = header.replaceAll("[^A-z_,]", UNDERSCORE);
 		HEADER = HEADER + ",VISIT_ID";
-		
+
 	}
-	public static int getMax(int A, int B)
-	{
-		if(A>=B)return A;
-		else return B;
+
+	public static int getMax(int A, int B) {
+		if (A >= B)
+			return A;
+		else
+			return B;
 	}
-	private static void writeMax(FileOutputStream oStream) throws IOException
-	{
-		String output="";
+
+	private static void writeMax(FileOutputStream oStream) throws IOException {
+		String output = "";
 		int index = 0;
-		for(; index < NUM_FIELDS-1; index++)
-		{
-			
-			output= output + max_lengths[index].toString() + COMMA;
+		for (; index < NUM_FIELDS - 1; index++) {
+			if (max_lengths[index] != null) {
+				output = output + max_lengths[index].toString() + COMMA;
+			} else {
+				output = output + "0" + COMMA;
+			}
 		}
-		output= output + max_lengths[index].toString(); // so no trailing comma
+		if (max_lengths[index] != null) {
+			output = output + max_lengths[index].toString();
+		} else {
+			output = output + "0";
+		}
+		// so no trailing comma
 		oStream.write(output.getBytes());
 		oStream.write(NEW_LINE.getBytes());
 		oStream.flush();
 	}
-	static void  checkMaxValues(String[] value)
-	{
-		for(int index = 0; index < NUM_FIELDS; index++)
-		{
-			if(max_lengths[index] == null)
-			{
+
+	static void checkMaxValues(String[] value) {
+		for (int index = 0; index < NUM_FIELDS; index++) {
+			if (max_lengths[index] == null) {
 				max_lengths[index] = new Integer(0);
 			}
-			if(index != CHF_CMPLN){
-			max_lengths[index] = getMax(max_lengths[index], value[index].length());
+			if (index != CHF_CMPLN) {
+				max_lengths[index] = getMax(max_lengths[index],
+						value[index].length());
 			}
 		}
 
-
 	}
+
 	public static int[] parseComplaint(String infile, String outfile)
 			throws Exception {
 		File in_file = new File(infile);
@@ -257,22 +280,42 @@ public class DelimitedFileParserClob implements IFileParser {
 		FileOutputStream errors = null;
 		boolean bad;
 		int[] out = new int[2];
-		Integer  visitID = new Integer(0);
+		Integer visitID = new Integer(0);
 		String line;
 		try {
 			is = new FileInputStream(in_file);
 			oStream = new FileOutputStream(out_file);
 			errors = new FileOutputStream(error_log);
 			sc = new Scanner(is).useDelimiter(NEW_RECORD);
-			//oStream.write(HEADER.getBytes());
+			// oStream.write(HEADER.getBytes());
 			// sc.useDelimiter("\\n\\d*[^A-z][,]");
+		
+				error_log.createNewFile();
+				errors.write(HEADER.getBytes());
+				errors.write(NEW_LINE.getBytes());
+				errors.flush();
+				
+			
 			while (sc.hasNext()) {
 				line = sc.next();
 				bad = false;
 				String[] value = line.split(COMMA);
-				if (value.length == NUM_FIELDS) {
+				if (value.length < NUM_FIELDS) {
+					String[] temp = new String[NUM_FIELDS];
+					for(int i = 0; i < NUM_FIELDS; i ++)
+					{
+						if(i < value.length){
+						temp[i]= value[i];
+						}else {
+							temp[i]=BLANK;
+						}
+					}
+					value = temp;
+				}
+				if(value.length == NUM_FIELDS){
 					visitID++;
-					TMHVisit current = new TMHVisit(); // will be used later to generate xml
+					TMHVisit current = new TMHVisit(); // will be used later to
+														// generate xml
 					current.setHSPMDNUM(value[HSP_MD_NUM]);
 					current.setEncounterdiagdescr(value[ENCOUNTERDIAGDESCR]);
 
@@ -285,22 +328,20 @@ public class DelimitedFileParserClob implements IFileParser {
 					current.setSurgHistProc(value[SURG_HIST_PROC]);
 					current.setVisitId(visitID.toString());
 					String[] complaints = value[CHF_CMPLN].split(AND);
-					String head = value[HSP_MD_NUM]+COMMA;
-							
-					String tail =COMMA+
-									value[ENCOUNTERDIAGDESCR]+COMMA+
-									value[LCHL_NTK]+COMMA+
-									value[SMK_HW_MCH]+COMMA+
-									value[SMKNG_FRMR_SMKRS_QT_TM]+COMMA+
-									value[SMKNG_STTS]+COMMA+
-									value[TBCC_YRS_FS]+COMMA+
-									value[TBCC_YRS_FS_DT]+COMMA+
-									value[SURG_HIST_PROC]+COMMA+
-									visitID.toString();
+					String head = value[HSP_MD_NUM] + COMMA;
+
+					String tail = COMMA + value[ENCOUNTERDIAGDESCR] + COMMA
+							+ value[LCHL_NTK] + COMMA + value[SMK_HW_MCH]
+							+ COMMA + value[SMKNG_FRMR_SMKRS_QT_TM] + COMMA
+							+ value[SMKNG_STTS] + COMMA + value[TBCC_YRS_FS]
+							+ COMMA + value[TBCC_YRS_FS_DT] + COMMA
+							+ value[SURG_HIST_PROC] + COMMA
+							+ visitID.toString();
 					checkMaxValues(value);
 					for (String complaint : complaints) {
 						current.getChfCmpln().add(complaint);
-						max_lengths[CHF_CMPLN]= getMax(complaint.length(), max_lengths[CHF_CMPLN]);
+						max_lengths[CHF_CMPLN] = getMax(complaint.length(),
+								max_lengths[CHF_CMPLN]);
 						String text = head + complaint + tail;
 						oStream.write(text.getBytes());
 						oStream.write(NEW_LINE.getBytes());
@@ -315,6 +356,7 @@ public class DelimitedFileParserClob implements IFileParser {
 
 				if (bad) {
 					out[BAD_COUNT]++;
+					
 					errors.write(line.getBytes());
 					errors.write(NEW_LINE.getBytes());
 					errors.flush();
@@ -347,6 +389,7 @@ public class DelimitedFileParserClob implements IFileParser {
 		}
 		return out;
 	}
+
 	public static int[] removeDuplicateRecords(String infile, String outfile)
 			throws Exception {
 		int[] out = new int[2];
@@ -367,31 +410,36 @@ public class DelimitedFileParserClob implements IFileParser {
 			writeMax(oStream);
 			oStream.write(HEADER.getBytes());
 			oStream.write(NEW_LINE.getBytes());
-			
+
 			oStream.flush();
 			ArrayList<String> records = new ArrayList<String>();
+		
+				error_log.createNewFile();
+				errors.write(HEADER.getBytes());
+				errors.write(NEW_LINE.getBytes());
+				errors.flush();
+				
+			
 			while (sc.hasNextLine()) {
 				line = sc.nextLine();
-				String crushed = line.replaceAll(SPACE,BLANK);
+				String crushed = line.replaceAll(SPACE, BLANK);
 				bad = false;
-				
-				if(records.contains(crushed))
-				{
+
+				if (records.contains(crushed)) {
 					bad = true;
-				}
-				else
-				{
+				} else {
 					records.add(crushed);
 				}
-				
+
 				if (bad) {
+					
 					errors.write(line.getBytes());
 					errors.write(NEW_LINE.getBytes());
 					errors.flush();
 					out[BAD_COUNT]++;
 				} else {
-					if(DEBUG){
-					line = line + out[GOOD_COUNT];
+					if (DEBUG) {
+						line = line + out[GOOD_COUNT];
 					}
 					oStream.write(line.getBytes());
 					oStream.write(NEW_LINE.getBytes());
@@ -400,7 +448,7 @@ public class DelimitedFileParserClob implements IFileParser {
 				}
 				out[GOOD_COUNT]++;
 			}
-			
+
 		} catch (Exception e) {
 
 			throw (e);
@@ -444,6 +492,12 @@ public class DelimitedFileParserClob implements IFileParser {
 			oStream = new FileOutputStream(out_file);
 			errors = new FileOutputStream(error_log);
 			sc = new Scanner(is).useDelimiter(NEW_RECORD);
+		
+				error_log.createNewFile();
+				errors.write(HEADER.getBytes());
+				errors.write(NEW_LINE.getBytes());
+				errors.flush();
+				
 			while (sc.hasNextLine()) {
 				line = sc.nextLine();
 				bad = false;
@@ -460,13 +514,14 @@ public class DelimitedFileParserClob implements IFileParser {
 					bad = true;
 				}
 				if (bad) {
+					
 					errors.write(line.getBytes());
 					errors.write(NEW_LINE.getBytes());
 					errors.flush();
 					out[BAD_COUNT]++;
 				} else {
-					if(DEBUG){
-					line = line + out[GOOD_COUNT];
+					if (DEBUG) {
+						line = line + out[GOOD_COUNT];
 					}
 					oStream.write(line.getBytes());
 					oStream.write(NEW_LINE.getBytes());
@@ -521,15 +576,20 @@ public class DelimitedFileParserClob implements IFileParser {
 			sc = new Scanner(is).useDelimiter(NEW_RECORD);
 			// sc.useDelimiter("\\n\\d*[^A-z][,]");
 			cleanHeader(sc.next());
+			
+				error_log.createNewFile();
+				errors.write(HEADER.getBytes());
+				errors.write(NEW_LINE.getBytes());
+				errors.flush();
+				
+			
 			while (sc.hasNext()) {
 				line = sc.next();
-				line = line.replaceAll(ILLLEGAL_CHARS,SPACE);
+				line = line.replaceAll(ILLLEGAL_CHARS, SPACE);
 				bad = false;
 				int commas = countInstances(line, COMMA);
+
 				if (commas != FIELD_BREAKS) {
-					clean_using_ntk(line);
-					commas = countInstances(line, COMMA);
-					if (commas != FIELD_BREAKS) {
 					bad = true;
 					char cr = 10;
 					char lf = 13;
@@ -539,13 +599,31 @@ public class DelimitedFileParserClob implements IFileParser {
 					line = clearCommas(line);
 					commas = countInstances(line, COMMA);
 					if (commas != FIELD_BREAKS) {
-						if (!error_log.exists()) {
-							error_log.createNewFile();
+						line = clean_using_ntk(line);
+						commas = countInstances(line, COMMA);
+						if(commas < FIELD_BREAKS)
+						{
+							line = restore_ntk_comma(line);
+							commas = countInstances(line, COMMA);
 						}
-						out[BAD_COUNT]++;
-						errors.write(line.getBytes());
-						errors.write(NEW_LINE.getBytes());
-						errors.flush();
+						else if(commas != FIELD_BREAKS)
+						{
+							line = clear_using_end(line);
+							commas = countInstances(line, COMMA);
+						}
+							
+						//end
+						if (commas != FIELD_BREAKS) {
+
+							
+							out[BAD_COUNT]++;
+							errors.write(line.getBytes());
+							errors.write(NEW_LINE.getBytes());
+							errors.flush();
+						} // end
+						else {
+							bad = false;
+						}
 					} else {
 						bad = false;
 					}
@@ -556,7 +634,7 @@ public class DelimitedFileParserClob implements IFileParser {
 					oStream.write(NEW_LINE.getBytes());
 					oStream.flush();
 				}
-				}
+
 				out[GOOD_COUNT]++;
 
 			}
@@ -628,31 +706,88 @@ public class DelimitedFileParserClob implements IFileParser {
 
 	}
 
-	public static void clean_using_ntk(String line)
+	public static String restore_ntk_comma(String line)
 	{
+		line = line.toUpperCase();
 		int start = line.indexOf(COMMA);
-		int end =-1;
-		for(int index =0; index < NTK_TYPE.length; index ++)
-		{
-			int temp = line.indexOf(':', start+1);
-			if(temp > end ) end = temp;
+		int end = -1;
+		for (int index = 0; index < NTK_TYPE.length; index++) {
+			int temp = line.indexOf(NTK_TYPE[index], start + 1);
+			if (temp > end)
+				end = temp;
 		}
-		if(end > start)
-		{
-			String front = line.substring(0,start);
-			String middle = line.substring(start,end);
+		if(end > 1){
+			String front = line.substring(0,end);
 			String tail = line.substring(end);
-			if(countInstances(middle,COMMA)>1)
+		line = front + COMMA + tail;
+		}
+		return line;
+	}
+	public static String clear_using_end(String line)
+	{
+		int tail_index=-1;
+		int count =0;
+		String tail ="";
+		String head = line;
+		for(count =0; count < TAIL_LENGTH; count++)
+		{
+			tail_index = head.lastIndexOf(COMMA);
+			if(tail_index < 1)
 			{
-				//removes all commas merging chf cmpl and encouterDiaDesc
-				middle = middle.replaceAll(COMMA, AND) + COMMA;
+				break;
+			}
+			tail = head.substring(tail_index) + tail;
+			head = head.substring(0,tail_index);
+		}
+		if(count == TAIL_LENGTH)
+		{
+			int first = head.indexOf(COMMA);
+			String front = head.substring(0,first +1);
+			String middle = head.substring(first);// 
+			middle = middle.replaceAll(COMMA, AND);// is now chfcomplnt
+			line = front + middle + COMMA + COMMA+ tail;
+			
+		}
+		return line;
+	}
+	public static String clean_using_ntk(String line) {
+		line = line.toUpperCase();
+	 
+		int start = line.indexOf(COMMA);
+		int end = -1;
+		for (int index = 0; index < NTK_TYPE.length; index++) {
+			int temp = line.indexOf(NTK_TYPE[index], start + 1);
+			if (temp > end){
+				end = temp;
+				String sub = line.substring(temp);
+				int nextComma = sub.indexOf(COMMA);
+				if(NTK_TYPE[index].length()<= nextComma && NTK_TYPE[index].length() +3 >= nextComma){
+					break;
+				}
+			}
+		}
+		if (end > start) {
+			String front = line.substring(0, start+1);
+			String middle = line.substring(start+1, end);
+			String tail = line.substring(end);
+			if (countInstances(middle, COMMA) > 1) {
+				// removes all commas merging chf cmpl and encouterDiaDesc
+				String [] parts = middle.split(COMMA);
+				middle = BLANK;
+				for(int i = 0; i < parts.length ; i ++)
+				{
+					if(parts[i].length()>1)
+					{
+						middle = middle + AND + parts[i];
+					}
+				}
+				middle = middle + COMMA;
 			}
 			line = front + middle + tail;
 		}
-		
-		
-		
+		return line;
 	}
+
 	public void setDelimiters(List<Object> del) {
 		Object[] input = del.toArray();
 		Delimiters = new String[input.length];
